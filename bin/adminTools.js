239957ff82b9322facd0ed8438fa990f528fc4bd
Jeffrey Gebhardt <jeff@gebbey.com>
2016-08-09 13:35:56 -0700

    updated admin tool

--- bin/adminTools.js
+++ bin/adminTools.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'; //eslint-disable-line
 
 const request = require('request');
 const vorpal = require('vorpal');
 const prettyjson = require('prettyjson');
 const Pie = require('cli-pie');
+const randomcolor = require('randomcolor');
+const EventSource = require('eventsource');
 
 const PollBaseUrl = 'https://real-votes.herokuapp.com/api/poll/';
 const VoteBaseUrl = 'https://real-votes.herokuapp.com/api/vote/';
-
 
 console.log('Hello welcome to the real-votes admin console.');
 
 const cli = vorpal();
 
 cli
-  .command('createPoll', 'Creates a new poll')
+  .command('addPoll', 'Creates a new poll')
   .action(function(args, callback) {
     this.prompt([
       {
@@ -48,8 +49,11 @@ cli
       };
 
       request.post(options, (err) => {
-        if (err) return this.log(err);
-        this.log('Success!');
+        if (err) {
+          this.log(err);
+          return callback();
+        }
+        this.log('Successfully created a poll.');
         callback();
       });
     });
@@ -80,41 +84,120 @@ cli
       };
 
       request.put(options, (err) => {
-        if (err) return this.log(err);
-        this.log('Success!');
+        if (err) {
+          this.log(err);
+          return callback();
+        }
+        this.log('Successfully updated poll.');
         callback();
       });
     });
   });
 
 cli
-  .command('showAllPolls', 'Shows all polls')
+    .command('deletePoll', 'deletes one poll')
+    .action(function(args, callback) {
+      this.prompt([
+        {
+          type: 'input',
+          name: 'id',
+          message: 'Please enter the polls id you want to delete: ',
+        },
+      ], (answers) => {
+        const options = {
+          url: PollBaseUrl + answers.id,
+          auth: {
+            username: 'admin',
+            password: process.env.PASSWORD,
+          },
+        };
+
+        request.delete(options, (err) => {
+          if (err) {
+            this.log(err);
+            return callback();
+          }
+          this.log('Successfully deleted poll.');
+          callback();
+        });
+      });
+    });
+
+cli
+  .command('viewAllPolls', 'Shows all polls')
   .action(function(args, callback) {
     request.get(PollBaseUrl, (err, res, body) => {
-      if (err) return this.log(err);
+      if (err) {
+        this.log(err);
+        return callback();
+      }
       this.log(prettyjson.render(JSON.parse(body)));
       callback();
     });
   });
 
+function renderTally(results) {
+  const chart = new Pie(10, [], { legend: true });
+
+  const colorPalette = randomcolor({
+    format: 'rgbArray',
+    seed: results.seed,
+    count: results.choices.length,
+  });
+
+  results.choices.forEach((choice, index) => {
+    chart.add({
+      label: choice,
+      value: results.votes[choice] || 0,
+      color: colorPalette[index],
+    });
+  });
+
+  return chart.toString();
+}
+
 cli
   .command('showResults', 'Show the results of the current poll')
   .action(function(args, callback) {
     request.get(`${VoteBaseUrl}tally`, (err, res, body) => {
-      if (err) return this.log(err);
+      if (err) {
+        this.log(err);
+        return callback();
+      }
       const results = JSON.parse(body);
-      const chart = new Pie(10, [], { legend: true });
-
-      Object.keys(results).forEach((key) => {
-        chart.add({
-          label: key,
-          value: results[key],
-        });
-      });
-
-      this.log(chart.toString());
+      this.log(renderTally(results));
       callback();
     });
+  });
+
+cli
+  .command(
+    'showRealtimeResults',
+    'Show the results of the current poll and keep them updated in real time'
+  )
+  .action(function(args, callback) {
+    // Get the initial tally
+    request.get(`${VoteBaseUrl}tally`, (err, res, body) => {
+      if (err) {
+        this.log(err);
+        return callback();
+      }
+      const results = JSON.parse(body);
+      process.stdout.write('\u001bc');
+      this.log(renderTally(results));
+    });
+
+    // Subscribe to tally updates
+    const es = new EventSource(`${VoteBaseUrl}tally/stream`);
+
+    es.addEventListener('message', (e) => {
+      const data = JSON.parse(e.data);
+      if (data === 'heartbeat') return;
+
+      // Clear the console
+      process.stdout.write('\u001bc');
+      this.log(renderTally(JSON.parse(data)));
+    }, false);
   });
 
 cli
